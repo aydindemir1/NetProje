a200473ce4ddbf96186fd5e40488aa50ead088a5
aydindemir1 <aynndemr@gmail.com>
2024-05-10 16:25:04 +0300

    Generic class and method

---
+++ GenericClass/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+// Generic Stack sınıfı tanımı
+public class Stack<T>
+{
+    private T[] items;
+    private int top;
+
+    public Stack(int size)
+    {
+        items = new T[size];
+        top = -1;
+    }
+
+    public void Push(T item)
+    {
+        if (top == items.Length - 1)
+        {
+            throw new StackOverflowException("Stack is full");
+        }
+        items[++top] = item;
+    }
+
+    public T Pop()
+    {
+        if (top == -1)
+        {
+            throw new InvalidOperationException("Stack is empty");
+        }
+        return items[top--];
+    }
+
+    public int Count
+    {
+        get { return top + 1; }
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Stack<int> türünde bir nesne oluşturma
+        Stack<int> intStack = new Stack<int>(5);
+
+        // Stack'e değerler eklemek
+        intStack.Push(10);
+        intStack.Push(20);
+        intStack.Push(30);
+
+        // Stack'ten değerler çıkarmak ve yazdırmak
+        Console.WriteLine("Popped item: " + intStack.Pop());
+        Console.WriteLine("Popped item: " + intStack.Pop());
+
+        // Stack<string> türünde bir nesne oluşturma
+        Stack<string> stringStack = new Stack<string>(3);
+
+        // Stack'e değerler eklemek
+        stringStack.Push("Hello");
+        stringStack.Push("World");
+
+        // Stack'ten değerler çıkarmak ve yazdırmak
+        Console.WriteLine("Popped item: " + stringStack.Pop());
+    }
+}
---
+++ GenericMethod/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+public class Program
+{
+    // Generic metot tanımı
+    public static void Swap<T>(ref T a, ref T b)
+    {
+        T temp = a;
+        a = b;
+        b = temp;
+    }
+
+    static void Main(string[] args)
+    {
+        int num1 = 10;
+        int num2 = 20;
+
+        // Swap metodu çağrılıyor
+        Console.WriteLine("Before swap: num1 = " + num1 + ", num2 = " + num2);
+        Swap(ref num1, ref num2);
+        Console.WriteLine("After swap: num1 = " + num1 + ", num2 = " + num2);
+
+        string str1 = "hello";
+        string str2 = "world";
+
+        // Swap metodu çağrılıyor
+        Console.WriteLine("Before swap: str1 = " + str1 + ", str2 = " + str2);
+        Swap(ref str1, ref str2);
+        Console.WriteLine("After swap: str1 = " + str1 + ", str2 = " + str2);
+    }
+}
